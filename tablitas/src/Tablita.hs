7408b5bf6ec3d873766bdbbf7cfc94e2de362757
daniel <daniel@bogusemailserver.com>
2017-10-28 18:34:40 +0000

    docs

--- tablitas/src/Tablita.hs
+++ tablitas/src/Tablita.hs
@@ -2,6 +2,9 @@
 --   have a finite number of values.
 -- 
 --   The motivating use case was to represent verb conjugations.
+--
+--   Be careful with the number and size of the dimensions or you will risk a
+--   combinatorial explosion!
 module Tablita (
       -- * The Tablita data type
       Tablita
