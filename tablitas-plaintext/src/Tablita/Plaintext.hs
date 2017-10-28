c6adef31d7c6e1fb77a4f8474b3db744760f7755
daniel <daniel@bogusemailserver.com>
2017-10-28 20:57:39 +0000

    text dep

--- tablitas-plaintext/src/Tablita/Plaintext.hs
+++ tablitas-plaintext/src/Tablita/Plaintext.hs
@@ -1,3 +1,27 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
 module Tablita.Plaintext where
 
+import Data.Void
+import Data.Text (Text)
+import Text.Megaparsec
+import Generics.SOP (All)
+
+import Tablita
+
+type Parser = Parsec Void Text
+
+parse2DFile :: (Dimensions xs, All Read xs, xs ~ [x1,x2]) => String -> Tablita xs String
+parse2DFile = undefined
 
