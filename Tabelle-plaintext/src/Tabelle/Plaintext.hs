e09daccc85c0934a184bb3e98398d969dfb6aa37
daniel <daniel@bogusemailserver.com>
2017-11-29 22:33:32 +0000

    ...

--- Tabelle-plaintext/src/Tabelle/Plaintext.hs
+++ Tabelle-plaintext/src/Tabelle/Plaintext.hs
@@ -23,10 +23,14 @@ import Text.Megaparsec.Char
 
 type Parser = Parsec Void Text
 
+-- http://hackage.haskell.org/package/megaparsec-6.2.0/docs/Text-Megaparsec.html
+-- http://hackage.haskell.org/package/megaparsec-6.2.0/docs/Text-Megaparsec-Char.html
+-- http://hackage.haskell.org/package/parser-combinators-0.2.0/docs/Control-Applicative-Combinators.html
+
 parser2D :: (Ord d1,Ord d2) => (Parser d1,Parser d2) -> Parser r -> Parser [((d1,d2),r)]
 parser2D (d1,d2) r = do
     -- cols <- space1 *> sepBy liftA2 (\cs c -> cs ++ [c]) (some (d2 <* space1)) (d2 <* space <* eol)   
-    cols <- space1 *> sepBy liftA2 (\cs c -> cs ++ [c]) (some (d2 <* space1)) (d2 <* space <* eol)   
+    cols <- space1 *> sepEndBy1 d1 space1 <* eol
     return []
      
 --parse2DFile :: (Dimensions xs, All Read xs, xs ~ [x1,x2]) => String -> Tabelle xs String
